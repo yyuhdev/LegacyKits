e2ed6af09d978fff0b5552cfb4775b79f99f9ebe
Omnikeck <dustinohlemann@gmail.com>
2024-08-19 20:12:06 +0200

    Fixed kit copying gui

--- src/main/java/club/revived/menus/misc/KitCopySelector.java
+++ src/main/java/club/revived/menus/misc/KitCopySelector.java
@@ -1,16 +1,13 @@
 package club.revived.menus.misc;
 
-import club.revived.LegacyKits;
 import club.revived.cache.KitCache;
 import club.revived.framework.inventory.InventoryBuilder;
-import club.revived.menus.KitEditor;
 import club.revived.objects.Kit;
 import club.revived.objects.KitHolder;
 import club.revived.storage.DatabaseManager;
 import dev.manere.utils.item.ItemBuilder;
 import dev.manere.utils.text.color.TextStyle;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
@@ -19,8 +16,10 @@ public class KitCopySelector extends InventoryBuilder {
     public KitCopySelector(Player player, int id) {
         super(36, Component.text(player.getName() + "'s Kits"));
         for (int x = 10; x < 17; x++) {
-            if (x-9 == id) continue;
-            int kitId = x-8;;
+            if (x-9 == id){
+                continue;
+            }
+            int kitId = x-9;;
             setItem(x, ItemBuilder.item(Material.BOOK)
                     .name(TextStyle.style("<#cdd6fa>\uD83C\uDFF9 Custom Kit <kit>"
                             .replace("<kit>", String.valueOf(kitId))))
@@ -38,8 +37,10 @@ public class KitCopySelector extends InventoryBuilder {
             });
         }
         for (int x = 19; x < 26; x++) {
-            if (x-9 == id) continue;
-            int kitId = x-8;;
+            if (x-9 == id){
+                continue;
+            }
+            int kitId = x-9;
             setItem(x, ItemBuilder.item(Material.BOOK)
                     .name(TextStyle.style("<#cdd6fa>\uD83C\uDFF9 Custom Kit <kit>"
                             .replace("<kit>", String.valueOf(kitId))))
