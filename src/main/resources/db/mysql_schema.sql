8924296ab18ad320d4af39b6148992edee8a750a
Omnikeck <dustinohlemann@gmail.com>
2024-09-11 19:12:12 +0200

    Super Commit

--- src/main/resources/db/mysql_schema.sql
+++ src/main/resources/db/mysql_schema.sql
@@ -20,12 +20,31 @@ CREATE TABLE IF NOT EXISTS enderchests (
     ) CHARACTER SET utf8
     COLLATE utf8_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS stats (
+    owner VARCHAR(36) NOT NULL PRIMARY KEY,
+    kills INT,
+    deaths INT,
+    totemsUsed INT
+    ) CHARACTER SET utf8
+    COLLATE utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS ffaKits (
+    owner VARCHAR(36) NOT NULL PRIMARY KEY,
+    mace_kit TEXT NOT NULL,
+    uhc_kit TEXT NOT NULL
+    ) CHARACTER SET utf8
+    COLLATE utf8_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS settings (
     uuid VARCHAR(36) NOT NULL PRIMARY KEY,
     kit INT NOT NULL,
     kitMessages BOOLEAN NOT NULL,
     smartAutoKit BOOLEAN NOT NULL,
     singleClickKitRoom BOOLEAN NOT NULL,
-    autokit BOOLEAN NOT NULL
+    autokit BOOLEAN NOT NULL,
+    joinmessages BOOLEAN NOT NULL,
+    tpaRequests BOOLEAN NOT NULL,
+    nightvision BOOLEAN NOT NULL,
+    tpahereRequests BOOLEAN NOT NULL
 ) CHARACTER SET utf8
     COLLATE utf8_unicode_ci;
